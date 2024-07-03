bc2d7283e2cd977e65fd1e562895c2998cab3f5e
tund <tund@gemvietnam.com>
2024-07-03 17:32:16 +0700

    init project

---
+++ src/components/todo/TodoItem.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  idx: Number,
+  title: String,
+  onRemoveTodo: Function
+})
+</script>
+
+<template>
+  <span key="idx">
+    {{ title }}
+    <button @click="onRemoveTodo(idx)">x</button>
+    <!-- <button @click.stop="$emit('remove-todo', idx)">Click Me</button> -->
+  </span>
+</template>
---
+++ src/views/TodoView.vue
@@ -0,0 +1,40 @@
+<template lang="">
+  <div>
+    <h2>Todo app</h2>
+    <div>
+      <input v-model="teachName" />
+      <button @click="onAddTodo">Add</button>
+    </div>
+    <li v-for="(i, index) in list">
+      <TodoItem
+        :idx="index"
+        :title="i"
+        :onRemoveTodo="onRemoveTodo"
+        @remove-todo.once="onRemoveTodo"
+      />
+    </li>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive, computed } from 'vue'
+import TodoItem from '../components/todo/TodoItem.vue'
+
+// state
+const teachName = ref('')
+const list = ref(['ReactJS', 'VueJS', 'Angular'])
+
+// function
+function onAddTodo() {
+  if (!teachName.value) return
+
+  list.value.push(teachName.value)
+  teachName.value = ''
+}
+
+function onRemoveTodo(idx) {
+  if (!idx) return
+
+  list.value.splice(idx, 1)
+}
+</script>
