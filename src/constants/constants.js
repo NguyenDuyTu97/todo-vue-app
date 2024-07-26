2effff615db20757f3d52098afcb75e8e823d0e4
tund <tund@gemvietnam.com>
2024-07-26 15:23:04 +0700

    feature cookie

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,3 @@
 const API_URL = 'https://66430b853c01a059ea2140ca.mockapi.io'
-export { API_URL }
+const API_LOCAL_URL = 'http://localhost:8080'
+export { API_URL, API_LOCAL_URL }
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,89 @@
 <template lang="">
   <div>Login page</div>
-  <GoogleLogin :callback="callback" />
+
+  <form @submit.prevent="onLogin">
+    <label for="first">
+      Username:
+    </label>
+    <input 
+      type="text" 
+      id="first" 
+      name="first" 
+      placeholder="Enter your Username" required
+      v-model="username"
+    >
+
+    <label for="password">
+      Password:
+    </label>
+    <input 
+      type="password"
+      id="password" 
+      name="password"
+      placeholder="Enter your Password" required
+      v-model="password"
+    >
+
+    <div class="wrap">
+      <button type="submit"
+      >
+        Submit
+      </button>
+    </div>
+</form>
+
+<button type="button" @click="onClickMe">
+  Click me
+</button>
+
+  <!-- <GoogleLogin :callback="callback" /> -->
 </template>
 <script setup>
+import { API_LOCAL_URL } from '@/constants/constants';
+import { ref } from 'vue';
 import { decodeCredential } from 'vue3-google-login'
+
+const username = ref('nguyenvana@gmail.com');
+const password = ref('123456a@1')
+
+
+const onLogin = async()=>{
+  const url = API_LOCAL_URL + '/login'
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      withCredentials: true,
+      credentials: 'include',
+      body: JSON.stringify({username: username.value,password: password.value})
+    })
+    // if (!response.ok) {
+    //   throw new Error(`Response status: ${response.status}`)
+    // }
+
+    // const res = await response.json()
+    // if (res) {
+    //   users.value = res
+    // }
+  } catch (error) {
+    console.error(error.message, 'error message')
+  }
+}
+
+const onClickMe = async() =>{
+  // console.log("onClickMe 4")
+  await fetch(`${API_LOCAL_URL}/test`,{
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      withCredentials: true,
+      credentials: 'include',
+    })
+}
 
 const callback = (response) => {
   // This callback will be triggered when the user selects or login to
@@ -11,5 +91,6 @@ const callback = (response) => {
   const userData = decodeCredential(response.credential)
   console.log('Handle the userData', userData)
 }
+
 </script>
 <style lang=""></style>
